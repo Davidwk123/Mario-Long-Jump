4e4d7a22bd9f5d6beece9103762b69b1ee02149c
David W.K <dwelchkeliihoomalu@gmail.com>
2023-11-25 14:03:37 -1000

    Added a custom character mov. component
    Learning how to implement character movement component, so that I can implement custom crouch/long jump features

---
+++ Source/MarioLongJump/MarioLongJumpMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "MarioLongJumpMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MARIOLONGJUMP_API UMarioLongJumpMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
